c3806e7a693079ee946e30d7155c63e29a75e67d
wildman <wildman@gmail.com>
2020-09-06 13:57:05 -0700

    initial commit

---
+++ process_receipts.py
@@ -0,0 +1,24 @@
+import os
+import glob
+import json
+import shutil
+
+try:
+    os.mkdir('./processed')
+except OSError:
+    print("'processed' directory already exists")
+
+receipts = glob.glob('./new/receipt-[0-9]*.json')
+subtotal = 0.0
+
+for path in receipts:
+    with open(path) as f:
+        content = json.load(f)
+        subtotal += float(content['value'])
+    name = path.split("/")[-1]
+    destination = "./processed/" + name
+    shutil.move(path, destination)
+    print("Moved " + path + " to " + destination)
+
+print("Receipt subtotal: $%.2f" % subtotal)
+
