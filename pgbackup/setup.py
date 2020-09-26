85a445daaee00e142bc62a4c013f43a552905c9c
Nick <wildman@gmail.com>
2020-09-26 21:07:09 +0000

    initial commit

---
+++ pgbackup/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open('README.rst', encoding='UTF-8') as f:
+    readme = r.read()
+setup(
+    name="pgbackup",
+    version="0.1",
+    description='Database backups locally or to AWS s3',
+    long_description=readme,
+    author='Nick',
+    author_email='wildman@gmail.com',
+    install_requires=[],
+    packages=find_packages('src'),
+    package_dir={'': 'src'}
+)
+
