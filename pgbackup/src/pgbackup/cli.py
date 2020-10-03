178bbbe3c984e90a75e44141f3323bfb9601198c
Nick <wildman@gmail.com>
2020-10-03 22:14:54 +0000

    fixed typo

--- pgbackup/src/pgbackup/cli.py
+++ pgbackup/src/pgbackup/cli.py
@@ -28,7 +28,7 @@ def main():
     dump = pgdump.dump(args.url)
     if args.driver == 's3':
         client = boto3('s3')
-        storage.s3_client, dump.stdout, args.destination, 'example.sql')
+        storage.s3(client, dump.stdout, args.destination, 'example.sql')
     else:
         outfile = open(args.destination, 'wb')
         storage.local(dump.stdout, outfile)
